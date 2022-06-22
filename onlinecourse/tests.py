ead14d325b740569185daf6bd6be85232dc75a90
OhmmyD <ohm.doungkhae@gmail.com>
2022-06-22 09:59:49 +0000

    changes made from theia lab environment

--- onlinecourse/tests.py
+++ onlinecourse/tests.py
@@ -1,3 +1,28 @@
+import unittest
 from django.test import TestCase
 
-# Create your tests here.
+class TestCorrectAnswers(TestCase):
+
+    def test1(self):
+
+        # Database populated with 4 correct answers
+        # Test for 4 correct answers
+        self.assertEqual(Choice.Objects.filter(is_correct = True).count(), 4)
+
+        # Database populated with 4 correct answers
+        # Test that not all 8 choices are correct answers
+        self.assertNotEqual(Choice.Objects.filter(is_correct = True).count(), 8)
+
+class TestIncorrectAnswers(TestCase):
+
+    def test1(self):
+
+        # Database populated with 4 incorrect answers
+        # Test for 4 incorrect answers
+        self.assertEqual(Choice.Objects.filter(is_correct = False).count(), 4)
+
+        # Database populated with 4 incorrect answers
+        # Test that not all 8 choices are incorrect answers
+        self.assertNotEqual(Choice.Objects.filter(is_correct = True).count(), 8)
+
+unittest.main()
